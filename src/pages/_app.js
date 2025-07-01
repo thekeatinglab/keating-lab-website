3901331f5ea80e0a97765c9287903420b7ee9445
theomthakur <theomthakur@gmail.com>
2025-07-01 10:33:03 -0400

    Google Search Console meta tag verification changes.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="zyjCR1hAnFX_brP8iWxnh1pkWYguc1oK6_4XrMfKFqs" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
